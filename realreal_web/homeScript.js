579577564ccbf2da2101abf79e1fb16dec2889c8
sad7898 <siradhee_k@hotmail.com>
2020-07-07 15:15:23 +0700

    added animejs dropdown not finished

--- realreal_web/homeScript.js
+++ realreal_web/homeScript.js
@@ -1,3 +1,9 @@
 
 var button = document.getElementById("getBodyHeight");
 button.addEventListener("click", () => (console.log($("body").width())));
+
+
+var slideRight = anime({
+	targets: button,
+	translateX: 200
+});
