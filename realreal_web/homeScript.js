a116f124470bed31950e1bb5e4482350d26445dc
sad7898 <siradhee_k@hotmail.com>
2020-07-08 17:09:49 +0700

    main page and loading page added

--- realreal_web/homeScript.js
+++ realreal_web/homeScript.js
@@ -1,9 +1,57 @@
 
-var button = document.getElementById("getBodyHeight");
-button.addEventListener("click", () => (console.log($("body").width())));
+var main = document.querySelector(".main div");
+var playButton = document.querySelector(".goPlay button");
+var outer = document.querySelector(".outer");
+var inner = document.querySelector(".inner");
+
+function displayMain(){
+	var height = $("body").width();
+	var loadOuter = anime({
+		targets: outer,
+		easing: "easeInSine",
+		rotate: 270,
+		borderRadius: ["50","250"],
+		borderWidth: ["20","40"],
+		scale: 1.5,
+		duration: 1000,
+		direction: "alternate",
+		loop: true
+	})
+	var loadInner = anime({
+		targets: inner,
+		easing: "easeInSine",
+		rotate: 270,
+		borderRadius: ["25","125"],
+		borderWidth: ["20","40"],
+		scale: 1.5,
+		duration: 1000,
+		direction: "alternate",
+		loop: true
+	})
 
 
-var slideRight = anime({
-	targets: button,
-	translateX: 200
-});
+	
+}
+displayMain();
+$(document).ready(function(){
+	setTimeout(function(){
+
+
+		$(".still-load").remove();
+		$(".load").toggleClass("load");
+		var mainSlideDown = anime({
+			targets: main,
+			translateY: 70,
+			opacity: ["0","1"],
+			easing: "linear"
+		});
+		var play = anime({
+			targets: playButton,
+			opacity:["0","1"],
+			delay: 500,
+			easing: "linear",
+			translateY: 20
+		});
+	},500);
+})
+
